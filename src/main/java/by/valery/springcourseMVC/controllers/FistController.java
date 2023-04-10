24cc1fa491338279cc9f8a1f62cf0a07c541419e
Valeriy-V-R <valeriy_v_r@mail.ru>
2023-04-10 12:11:42 +0300

    lesson 17. Организовали переход по стр. по ссылкам с помощью тега <a href=... </a>

--- src/main/java/by/valery/springcourseMVC/controllers/FistController.java
+++ src/main/java/by/valery/springcourseMVC/controllers/FistController.java
@@ -1,4 +1,21 @@
 package by.valery.springcourseMVC.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class FistController {
+
+    @GetMapping("/hello")
+    public String helloPage() {
+        return ("first/hello");
+    }
+
+    @GetMapping("goodbye")
+    public String goodByePage() {
+        return ("first/goodbye");
+    }
+
+
 }

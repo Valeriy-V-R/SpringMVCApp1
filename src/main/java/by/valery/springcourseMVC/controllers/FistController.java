d06bf219c67ecdb1cb3e8bf2b6cef95a9b27af20
Valeriy-V-R <valeriy_v_r@mail.ru>
2023-04-13 08:15:39 +0300

    lesson 18. Initial.

--- src/main/java/by/valery/springcourseMVC/controllers/FistController.java
+++ src/main/java/by/valery/springcourseMVC/controllers/FistController.java
@@ -1,6 +1,7 @@
 package by.valery.springcourseMVC.controllers;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FistController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return ("first/hello");
     }
 

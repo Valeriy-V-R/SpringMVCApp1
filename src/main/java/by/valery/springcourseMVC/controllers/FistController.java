079cb781d1bcb6c616846f88c3534d431edb245d
Valeriy-V-R <valeriy_v_r@mail.ru>
2023-04-13 11:53:07 +0300

    lesson 18. END.

--- src/main/java/by/valery/springcourseMVC/controllers/FistController.java
+++ src/main/java/by/valery/springcourseMVC/controllers/FistController.java
@@ -5,15 +5,29 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FistController {
 
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+
+    // Получение параметров запроса с помощью HttpServletRequest
+//    public String helloPage(HttpServletRequest request) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+//
+//        System.out.println("Hello, " + name + " " + surname);
+//
+//        return ("first/hello");
+//    }
+
+
+    // Получение параметров запроса с помощью @RequestParam
+    //
+      public String helloPage(@RequestParam(value = "name", required = false) String name,
+                                    @RequestParam (value = "surname", required = false) String surname) {
 
         System.out.println("Hello, " + name + " " + surname);
 

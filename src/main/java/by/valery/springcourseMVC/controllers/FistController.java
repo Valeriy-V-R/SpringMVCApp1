375430a01f3f65b7869a7fa37ac03179b88cb32c
Valeriy-V-R <valeriy_v_r@mail.ru>
2023-04-13 12:57:27 +0300

    lesson 20. Initial.

--- src/main/java/by/valery/springcourseMVC/controllers/FistController.java
+++ src/main/java/by/valery/springcourseMVC/controllers/FistController.java
@@ -3,6 +3,7 @@ package by.valery.springcourseMVC.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,9 +28,11 @@ public class FistController {
     // Получение параметров запроса с помощью @RequestParam
     //
       public String helloPage(@RequestParam(value = "name", required = false) String name,
-                                    @RequestParam (value = "surname", required = false) String surname) {
+                              @RequestParam (value = "surname", required = false) String surname,
+                              Model model) {
 
-        System.out.println("Hello, " + name + " " + surname);
+        //System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
 
         return ("first/hello");
     }

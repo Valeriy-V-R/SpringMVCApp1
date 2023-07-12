566c0bae3f82f98866167e910b2ec004f894f809
Valeriy-V-R <valeriy_v_r@mail.ru>
2023-07-12 10:29:00 +0300

    Initial and END

--- src/main/java/by/valery/springcourse/dao/PersonDAO.java
+++ src/main/java/by/valery/springcourse/dao/PersonDAO.java
@@ -6,6 +6,7 @@ package by.valery.springcourse.dao;
 import by.valery.springcourse.models.Person;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,34 +16,90 @@ public class PersonDAO {
     private static int PEOPLE_COUNT;
     private List<Person> people;
 
-    {
-        people = new ArrayList<>();
+    // Подключение к БД
+    private static final String URL ="jdbc:postgresql://localhost:5432/first_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
 
-        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@yahoo.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy", 34, "katy@gmail.com"));
+    //Соединение к бд с помощью JDBC
+    static private Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public List<Person> index() { // Отображает всех людей из списка
+    // Отображает всех людей из списка
+    public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+
+        // Объект Statement содержит в себе запрос к БД
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person"; // SQL запрос
+            statement.executeQuery(SQL);
+
+            //Принимаем результат запроса statement.executeQuery(SQL)
+            //и помещаем результат в спец класс ResultSet
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            //проходим по строкам и переводим их в java объект
+            while (resultSet.next()) {
+                Person person = new Person();
+
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setEmail(resultSet.getString("email"));
+                person.setAge(resultSet.getInt("age"));
+
+                people.add(person);
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return people;
     }
 
     public Person show(int id) {  // Отображает человека по ID
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse((null));
+//        return people.stream().filter(person -> person.getId() == id).findAny().orElse((null));
+    return null;
     }
 
     public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+//        person.setId(++PEOPLE_COUNT);
+//        people.add(person);
+
+        //Создаем SQL запрос к БД
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName() + "',"
+                    + person.getAge() + ",'" + person.getEmail() + "')";
+            // INSET INTO Person VALUES(1, 'Tom', 18, 'tom@mail.ru')
+            // Такой запрос может иметь ошибки. Есть способ сделать лучше
+
+            statement.executeUpdate(SQL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 
     public void update(int id, Person updatedPerson){
-        Person personToBEUpdated = show(id);
-        personToBEUpdated.setName(updatedPerson.getName());
-        personToBEUpdated.setAge(updatedPerson.getAge());
-        personToBEUpdated.setEmail(updatedPerson.getEmail());
+//        Person personToBEUpdated = show(id);
+//        personToBEUpdated.setName(updatedPerson.getName());
+//        personToBEUpdated.setAge(updatedPerson.getAge());
+//        personToBEUpdated.setEmail(updatedPerson.getEmail());
     }
 
     public void delete(int id){
